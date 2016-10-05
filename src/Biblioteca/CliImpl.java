f0d34ffc5c635338d10ed8b486f1683f5cd5f03c
Rodrigo Kovalski <rodrigolk22@ymail.com>
2016-10-05 15:34:35 -0300

    Primeiro passo para a humnidade

--- src/Biblioteca/CliImpl.java
+++ src/Biblioteca/CliImpl.java
@@ -10,6 +10,8 @@ import java.rmi.registry.*;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -18,22 +20,21 @@ import java.util.logging.Logger;
 public class CliImpl extends UnicastRemoteObject implements InterfaceCli {
     
     InterfaceServ interfaceServ;
+    public List consulta = null;
 
     public CliImpl(InterfaceServ interfaceServ) throws RemoteException {
         this.interfaceServ = interfaceServ;
     }
     
-    public void consultarLivro(int id)
+    public List consultarLivro(int id) throws RemoteException
     {
-        try {
-            interfaceServ.consultarLivro(1, this);
-        } catch (RemoteException ex) {
-            System.err.println(ex.toString());
-            Logger.getLogger(CliImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        
+        consulta = interfaceServ.consultarLivro(1, this);
+        System.out.println(consulta);
+        return consulta;
     }
     
-    public void exibir(int livroId) throws RemoteException{
+    public void exibir(int livroId) throws RemoteException {
         System.out.println(livroId);
     }
     
---
+++ src/Biblioteca/Livro.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Biblioteca;
+
+import java.io.Serializable;
+
+
+
+/**
+ *
+ * @author Rodrigo e Jordan
+ */
+
+public class Livro implements Serializable { 
+    
+    private int id;
+    
+    private int quantidade;
+
+    private String titulo;
+
+    private String autor;
+
+    private String categoria;
+
+    public Livro() { }
+
+    public int getId() {
+            return id;
+    }
+
+    public void setId(int id) {
+            this.id = id;
+    }
+    
+    public int getQuantidade() {
+            return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+            this.quantidade = quantidade;
+    }
+
+    public String getTitulo() {
+            return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+            this.titulo = titulo;
+    }
+
+    public String getAutor() {
+            return autor;
+    }
+
+    public void setAutor(String autor) {
+            this.autor = autor;
+    }
+    public String getCategoria() {
+            return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+            this.categoria = categoria;
+    }
+}
